bfa52e9dfe3d11df5928415bfd20ba73b75ca854
Will Gibson <willgibson602@gmail.com>
2026-02-26 13:25:45 -0500

    feat: migrate to Neon PostgreSQL + Prisma v7 adapter pattern

--- server/db.js
+++ server/db.js
@@ -1,4 +1,7 @@
+import 'dotenv/config';
 import { PrismaClient } from '../src/generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 export default prisma;
